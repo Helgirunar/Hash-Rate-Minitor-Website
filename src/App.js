a9203fa57d89c3f249c3f5ebc5ca93f9097fc58d
Helgirunar <Helgirunar93@gmail.com>
2020-02-12 15:11:48 +0000

    Template for the Hash Rate Monitor Website

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+export class App extends Component {
+  render() {
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 classname="App-title">Welcome to React</h1>
+            </header>
+            <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload
+            </p>
+        </div>
+    )
+  }
+};
+
+export default connect()(App);
